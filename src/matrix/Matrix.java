c6d5b46b57e4149529d44c4ee280be414159320d
Lino Coria Mendoza <l.coria@northeastern.edu>
2024-06-19 11:25:55 -0700

    Fixed line for matrix multiplication exception.

--- src/matrix/Matrix.java
+++ src/matrix/Matrix.java
@@ -40,7 +40,7 @@ public interface Matrix {
   boolean isSame(Matrix other);
 
   /**
-   * Get the element at the specified row and column values.
+   * Get the element at the specified row and column.
    *
    * @param row the row of the element
    * @param col the column of the element
